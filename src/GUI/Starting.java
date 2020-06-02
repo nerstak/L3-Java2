84c169e731100c06de3efa80e1fb66fbacae9d06
nerstak <roy.karst@gmail.com>
2020-06-02 17:25:17 +0200

    Better starting window

--- src/GUI/Starting.java
+++ src/GUI/Starting.java
@@ -13,10 +13,14 @@ public class Starting {
     public Label welcomeLabel;
     public GridPane gridPane;
     public Button startButton;
+    public Label descriptionLabel;
 
     @FXML
     private void initialize() {
         welcomeLabel.setAlignment(Pos.CENTER);
+        welcomeLabel.setText("Welcome to the weakest link!");
+        descriptionLabel.setAlignment(Pos.CENTER);
+        descriptionLabel.setText("You and 3 of your friends have been selected to this game. You will have to play until there is only one of you left...");
     }
 
     @FXML
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import oo.Players.SetPlayers;
 
@@ -32,7 +33,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         URL url = new File("resources/fxml/Starting.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
-        primaryStage.setTitle("WEAKEST LINK");
+        primaryStage.setTitle("Weakest Link - The new entertainment game for the whole family");
+        primaryStage.getIcons().add(new Image("file:resources/img/weakest-link-icon.png"));
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
