5040480f695f06ae55f537a32d18e2a8349c4177
Rhadamanthe0 <clemt.picot@gmail.com>
2020-06-04 12:21:29 +0200

    Added themeSelection interface

---
+++ src/Scenes/ThemeSelection/ThemeSelectionController.java
@@ -0,0 +1,51 @@
+package Scenes.ThemeSelection;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
+
+public class ThemeSelectionController {
+	@FXML
+	public VBox choicesContainer;
+	public ToggleGroup toggleGroup;
+
+	@FXML
+	private void initialize() {
+		toggleGroup = new ToggleGroup();
+		
+		ArrayList<String> filesNames = getFilesNameInFolder(new File("resources/json"));
+		for (String fileName : filesNames) {
+			ToggleButton tb = new ToggleButton(fileName.substring(0, 1).toUpperCase() + fileName.substring(1, fileName.length() - 5));
+			tb.setPrefWidth(10000);
+			tb.setToggleGroup(toggleGroup);
+			choicesContainer.getChildren().add(tb);
+		}
+	}
+	
+	@FXML
+	public void onPressedSelectButton (ActionEvent actionEvent) {
+		ToggleButton selectedToggle = (ToggleButton) toggleGroup.getSelectedToggle();
+		if (selectedToggle == null) return;
+		
+		String themeSelected = selectedToggle.getText();
+		File fileSelected = new File ("resources/" + themeSelected.toLowerCase() + ".json");
+		System.out.println(fileSelected);
+		
+		// TODO: what after selection?
+	}
+	
+	private ArrayList<String> getFilesNameInFolder (final File folder) {
+		ArrayList<String> filesNames = new ArrayList<String>();
+		for (final File fileEntry : folder.listFiles()) {
+			if (!fileEntry.isDirectory()) {
+				filesNames.add(fileEntry.getName());
+			}
+		}
+		return filesNames;
+	}
+}
