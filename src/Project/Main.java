b14f60400783647b86cf21dee198f35a931303c1
Rhadamanthe0 <clemt.picot@gmail.com>
2020-06-04 13:37:39 +0200

    Migrated Main content to Game

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -1,19 +1,9 @@
 package Project;
 
-import javafx.application.Application;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-import oo.Players.Player;
-import oo.Players.PlayerStatus;
-import oo.Players.SetPlayers;
-import oo.Questions.Themes;
+import oo.Game.Game;
 
-
-public class Main extends Application {
-    public static SetPlayers listPlayers;
-    public static Themes themes;
-    public static SceneManager sceneManager;
-    public static Player currentPlayer;
+public class Main {
+    public static Game game;
 
     public static void main(String[] args) {
         /*MCQ<String> m = new MCQ<>("lol", "mo", "kp", "jo", "mo");
@@ -26,27 +16,8 @@ public class Main extends Application {
         t.display();
         ListQuestions l = new ListQuestions("music");
         l.display();*/
-        listPlayers = new SetPlayers();
-        themes = new Themes();
 
-        // TMP - Should not be conserved like this, as we need to select players
-        do {
-            listPlayers.selectPlayer().setStatus(PlayerStatus.waiting);
-        } while (listPlayers.countPlayers(PlayerStatus.waiting) < 4);
-
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        // Setting window parameter
-        primaryStage.setTitle("Weakest Link - The new entertainment game for the whole family");
-        primaryStage.getIcons().add(new Image("file:resources/img/weakest-link-icon.png"));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-
-        // Scene manager
-        sceneManager = new SceneManager(primaryStage);
-        sceneManager.activate("Starting");
+    	game = new Game();
+    	game._start();
     }
 }
---
+++ src/oo/Game/Game.java
@@ -0,0 +1,73 @@
+package oo.Game;
+
+import Project.SceneManager;
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import oo.Players.Player;
+import oo.Players.PlayerStatus;
+import oo.Players.SetPlayers;
+import oo.Questions.Themes;
+
+public class Game extends Application {
+    private static SetPlayers listPlayers;
+    private static Themes themes;
+    private static SceneManager sceneManager;
+    private static Player currentPlayer;
+    private static Phase currentPhase;
+	
+	public Game () {
+        listPlayers = new SetPlayers();
+        themes = new Themes();
+	}
+	
+	public void _start() {
+		launch();
+	}
+
+	@Override
+	public void start (Stage primaryStage) throws Exception {
+		setWindowParameters(primaryStage);
+		
+        selectFourPlayersRandomly();
+
+        sceneManager = new SceneManager(primaryStage);
+        sceneManager.activate("Starting");
+	}
+	
+	private void setWindowParameters (Stage primaryStage) {
+        primaryStage.setTitle("Weakest Link - The new entertainment game for the whole family");
+        primaryStage.getIcons().add(new Image("file:resources/img/weakest-link-icon.png"));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+	}
+	
+	private void selectFourPlayersRandomly () {
+        do {
+            listPlayers.selectPlayer().setStatus(PlayerStatus.waiting);
+        } while (listPlayers.countPlayers(PlayerStatus.waiting) < 4);
+	}
+	
+//	Phases du jeu
+//	Le jeu de Question / Réponse à réaliser comprendra trois phases où 4 joueurs s’affrontent autour de
+//	questions sur 10 thèmes :
+//	Phase I :
+//	Dans cette phase, le jeu se déroule entre 4 joueurs choisis aléatoirement. Un thème parmi 10 est
+//	sélectionné automatiquement du tableau dans un ordre séquentiel (un indicateur du dernier thème
+//	sélectionné est mis à jour, après le choix du 10ème thème, on revient au premier).
+//	Une question de niveau facile est sélectionnée pour chacun des joueurs selon une politique RoundRobin (i.e de manière circulaire). Les 4 joueurs répondent à leurs questions séparément, le score est
+//	incrémenté de 2 si la réponse donnée est correcte.
+//	Phase II :
+//	Le jeu se déroule entre les trois joueurs gagnants de la phase I. Cette phase propose deux questions
+//	de niveau moyen pour chaque joueur (une question par thème choisi).
+//	A ce niveau, les questions porteront sur uniquement 6 thèmes choisis du tableau aléatoirement.
+//	A tour de rôle et de manière alternée, chaque joueur choisit un thème (ensuite un deuxième) qui est
+//	supprimé des choix aussitôt sélectionnés.
+//	Une question de niveau moyen (dans le thème choisi) est sélectionnée selon la politique Round-Robin
+//	et présentée au joueur.
+//	Le score du joueur qui donne la bonne réponse est incrémenté de 3.
+//	Phase III :
+//	Dans cette phase, le jeu se déroule entre les deux joueurs gagnants de la phase II. Les questions
+//	porteront sur trois thèmes choisis par le concepteur du jeu. Le score du joueur donnant la bonne
+//	réponse est incrémenté de 5. 
+}
