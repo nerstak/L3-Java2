23b86e20a3953e4ca4695e94ce82fbe88e20af90
Rhadamanthe0 <clemt.picot@gmail.com>
2020-06-07 15:46:17 +0200

    Removed useless obsolte commented code in Main

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -10,19 +10,8 @@ public class Main extends Application {
 	public static SceneManager sceneManager;
 
     public static void main(String[] args) {
-        /*MCQ<String> m = new MCQ<>("lol", "mo", "kp", "jo", "mo");
-        m.display();
-        System.out.println(m.checkAnswer("mo"));
-        Question<?> q = new Question(m, "mdr", Difficulty.easy);
-        q.display();
-
-        Themes t = new Themes();
-        t.display();
-        ListQuestions l = new ListQuestions("music");
-        l.display();*/
-
-    	game = new Game();
-		launch();
+        game = new Game();
+        launch();
     }
 
 
