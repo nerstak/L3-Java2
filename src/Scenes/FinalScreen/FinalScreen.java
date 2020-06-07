12c56bee3c7ac1092af95032703bf1a37a58c49e
nerstak <roy.karst@gmail.com>
2020-06-07 17:48:20 +0200

    FinalScreen more functional

--- src/Scenes/FinalScreen/FinalScreen.java
+++ src/Scenes/FinalScreen/FinalScreen.java
@@ -1,8 +1,15 @@
 package Scenes.FinalScreen;
 
 import Project.Main;
+import ProjectUtilities.Utilities;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import oo.Players.Player;
 import oo.Players.PlayerStatus;
 
@@ -11,9 +18,12 @@ import java.util.Vector;
 
 public class FinalScreen {
     private final Vector<Player> ranking;
-    public TableColumn playerName;
-    public TableColumn score;
-    public TableColumn time;
+    public TableColumn<Player, String> playerName;
+    public TableColumn<Player, String> score;
+    public TableColumn<Player, String> time;
+    public Button restartButton;
+    @FXML
+    private TableView<Player> personTable;
 
 
     public FinalScreen() {
@@ -24,6 +34,21 @@ public class FinalScreen {
 
     @FXML
     private void initialize() {
+        playerName.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getName()));
+        score.setCellValueFactory(c -> new SimpleStringProperty(
+                String.valueOf(c.getValue().getScore())
+        ));
+        time.setCellValueFactory(c -> new SimpleStringProperty(
+                Utilities.convertTimestampToString(c.getValue().getTimer())
+        ));
 
+
+        ObservableList<Player> observableList = FXCollections.observableArrayList(ranking);
+        personTable.setItems(observableList);
+    }
+
+    @FXML
+    public void restartGame(ActionEvent actionEvent) {
+        // TODO: Add stuff to restart
     }
 }
