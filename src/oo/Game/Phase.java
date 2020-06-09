2204c3947a0527ca7ec3cd75039d94ba6bffed10
nerstak <roy.karst@gmail.com>
2020-06-09 14:58:48 +0200

    Completing Dummy interface Phase

--- src/oo/Game/Phase.java
+++ src/oo/Game/Phase.java
@@ -1,5 +1,11 @@
 package oo.Game;
 
+/**
+ * Interface for Phase
+ * Not used, as we prefer centralizing everything in class Game, supported by the enum PhaseEnum
+ */
 public interface Phase {
     void SelectPlayers();
+
+    void gamePhase();
 }
